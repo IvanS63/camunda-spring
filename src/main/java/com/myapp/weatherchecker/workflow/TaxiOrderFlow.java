c945ea4b9c28bc3e177b424963266ad514e2de19
Ivan Semenov <semenovivan94@gmail.com>
2022-08-25 12:37:32 +0400

    Fixed delegates

--- src/main/java/com/myapp/weatherchecker/workflow/TaxiOrderFlow.java
+++ src/main/java/com/myapp/weatherchecker/workflow/TaxiOrderFlow.java
@@ -1,4 +1,22 @@
 package com.myapp.weatherchecker.workflow;
 
-public class TaxiOrderFlow {
+import com.myapp.weatherchecker.util.WorkflowLogger;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Named;
+
+@Named("taxiOrderFlow")
+public class TaxiOrderFlow implements JavaDelegate {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        WorkflowLogger.info(logger, "TAXI_ORDER", "Sending request for ordering taxi");
+        Thread.sleep(1000);
+        WorkflowLogger.info(logger, "TAXI_ORDER", "Driver accepted the request");
+    }
 }
--- src/main/java/com/myapp/weatherchecker/workflow/WeatherCheckerFlow.java
+++ src/main/java/com/myapp/weatherchecker/workflow/WeatherCheckerFlow.java
@@ -1,11 +1,15 @@
 package com.myapp.weatherchecker.workflow;
 
+import com.myapp.weatherchecker.util.WeatherProvider;
 import com.myapp.weatherchecker.util.WorkflowLogger;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Named;
+
+@Named("weatherCheckerFlow")
 public class WeatherCheckerFlow implements JavaDelegate {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -13,5 +17,7 @@ public class WeatherCheckerFlow implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         WorkflowLogger.info(logger, "WEATHER_CHECK", "Check weather outside");
+        int currentTemperature = WeatherProvider.getCurrentTemperature();
+        delegateExecution.setVariable("isCold", currentTemperature <= 0);
     }
 }
